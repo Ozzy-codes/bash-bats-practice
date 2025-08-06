4841740d858e906288635d9cecc767a7694818b9
Osvaldo Mesa <d3v.ozzy@gmail.com>
2025-08-06 08:27:11 -0400

    update common set up test path

--- test_script/test_helper/common-setup.bash
+++ test_script/test_helper/common-setup.bash
@@ -9,7 +9,7 @@ _common_setup() {
   # get the containing directory of this file
   # use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
   # as those will point to the bats executable's location or the preprocessed file respectively
-  PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 4>&1 && pwd )"
+  export PROJECT_DIR="$( cd "$( dirname "$BATS_TEST_DIRNAME" )" >/dev/null 2>&1 && pwd )"
   # make executables in src/ visible to PATH
-  PATH="$PROJECT_ROOT/src:$PATH"
+  PATH="$PROJECT_DIR/src:$PATH"
 }
